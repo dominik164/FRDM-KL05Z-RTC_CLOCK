33ad89d8f06843ccbce4047edd9defb2c9c44710
dominik164 <48433356+dominik164@users.noreply.github.com>
2021-03-31 20:21:27 +0200

    Delete src directory

--- src/buttons.h
+++
@@ -1,8 +0,0 @@
-#ifndef buttons_h
-#define buttons_h
-
-#include "frdm_bsp.h" 
-
-void buttons_init(void);
-uint8_t get_key(void);
-#endif
--- src/rtc.h
+++
@@ -1,7 +0,0 @@
-#ifndef rtc_h
-#define rtc_h
-
-
-void RTC_Init(void);
-
-#endif
